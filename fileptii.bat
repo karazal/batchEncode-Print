78327e990ea26d608fc3cc156ffc06224f99f8de
karazal <69127362+karazal@users.noreply.github.com>
2024-02-06 14:13:56 -0300

    Update fileptii.bat

--- fileptii.bat
+++ fileptii.bat
@@ -15,16 +15,3 @@ for /l %%t in (0,1,%~1) DO (
 
 pause>nul
 popd
-choice /C YN /N /M "Voce deseja deletar o data.hxd original?"
-if %errorlevel% == 2 (
-    call :no
-) else (
-    call :yes
-)
-
-:no
-exit
-
-:yes
-del data.hxd /q
-pause
